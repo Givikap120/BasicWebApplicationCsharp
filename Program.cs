8aa8753d121afbd3dc28dffa1b7f3a5f54e2bcbf
Givikap120 <givikap120@gmail.com>
2025-12-24 20:40:44 +0100

    Change the sql server settings

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(
+    options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
     )
 );
