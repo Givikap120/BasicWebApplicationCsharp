256f1b1950d95951b6b99c7653fa4d7cdb89e794
Givikap120 <givikap120@gmail.com>
2025-12-24 21:04:47 +0100

    Implement order

--- Domains/Order.cs
+++ Domains/Order.cs
@@ -1,6 +1,21 @@
 ﻿namespace BasicWebApplicationCsharp.Domains
 {
+    public enum OrderStatus
+    {
+        Pending,
+        Paid,
+        Shipped,
+        Delivered,
+        Cancelled
+    }
+
     public class Order
     {
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
+        public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+        public DateTime CreatedAt { get; private set; }
+        public decimal TotalPrice { get; private set; }
     }
 }
