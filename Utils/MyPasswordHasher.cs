3e602fe97bc85e90ed72be367ce86c02455b713e
Givikap120 <givikap120@gmail.com>
2025-12-25 23:50:28 +0100

    Add password hasher

---
+++ Utils/MyPasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using BasicWebApplicationCsharp.Domains;
+using Microsoft.AspNetCore.Identity;
+
+namespace BasicWebApplicationCsharp.Utils
+{
+    public class MyPasswordHasher : PasswordHasher<User>, IPasswordHasher
+    {
+        // We will pass user as null since we don't need it for our hashing
+#pragma warning disable CS8625
+        public string HashPassword(string password) => HashPassword(null, password);
+        public PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword) => VerifyHashedPassword(null, hashedPassword, providedPassword);
+#pragma warning restore CS8625
+    }
+}
